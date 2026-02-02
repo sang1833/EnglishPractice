79ddef5ee6cddd85b5ff206d9db2ccc19b59dab1
Nguyen Thanh Sang <lc21ar@gmail.com>
2026-02-02 19:06:53 +0700

    BE: Add new fe url

--- S4C_BE/src/Study4Clone.Api/Program.cs
+++ S4C_BE/src/Study4Clone.Api/Program.cs
@@ -94,10 +94,9 @@ builder.Services.AddCors(options =>
 {
     options.AddPolicy("AllowFrontend", policy =>
     {
-        policy.WithOrigins("http://localhost:3000", "http://localhost:5173", "http://localhost:4200")
+        policy.AllowAnyOrigin()
               .AllowAnyMethod()
-              .AllowAnyHeader()
-              .AllowCredentials();
+              .AllowAnyHeader();
     });
 });
 
